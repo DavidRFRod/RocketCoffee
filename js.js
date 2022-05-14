97503c735105bb06470bbd822fbd19a876cb1bbc
DavidRFRod <davidrapha@hotmail.com>
2022-05-13 22:51:47 -0300

    Starting put JS

--- js.js
+++ js.js
@@ -1,12 +1,19 @@
-const menu = document.getElementsByClassName('menu');
-const openBurger = document.querySelector('#open-buguer');
 
-
-function toggleMenu() {
-    
-    const nav = document.querySelector('nav');
-    nav.classList.add('active')
-    
+function openMenu(){
+    const menu = document.getElementById('menu-list');
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+        menu.style.transition = "2s ease"
+    }
 }
-
-openBurger.addEventListener('click', toggleMenu);
+// function toggleMenu() {
+//     var menuBox = document.getElementById('menu-box');    
+//     if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+//       menuBox.style.display = "none";
+//     }
+//     else { // if is menuBox hidden, display it
+//       menuBox.style.display = "block";
+//     }
+//   }
